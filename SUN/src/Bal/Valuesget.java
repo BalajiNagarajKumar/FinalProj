9c969e246e3361719654da41a887bef0656f68ce
Stark28 <31844579+Stark28@users.noreply.github.com>
2018-05-18 18:44:00 +0200

    valuesget class

---
+++ SUN/src/Bal/Valuesget.java
@@ -0,0 +1,20 @@
+package Bal;
+
+import java.sql.*;
+
+public class Valuesget {
+	Valuesget(String root, String balsun){
+
+	}
+
+				// JDBC driver name and database URL
+				private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+				private static final String DB_URL = "jdbc:mysql://localhost/";
+				private static final String DISABLE_SSL = "?useSSL=false";
+				//SSL stands for Security Sockets Layer. It says it is not secure to start without disabling it
+
+				// Database credentials
+				private static String USER;
+				private static String PASS;
+}
+
